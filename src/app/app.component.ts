5d4f752e8c3ec40abda4e74c301ac1f421ab0f24
Felipe Gavilan <elmejor_887@hotmail.com>
2018-12-02 14:33:54 -0400

    Changed app-root for app-raiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-raiz',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'angular-todo';
+
+
 }
